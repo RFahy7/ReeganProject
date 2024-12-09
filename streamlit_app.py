d04474aa2ef0566c7254536296efd8ddd654bfe2
RFahy7 <reegan.fahy7@mail.dcu.ie>
2024-12-09 15:36:10 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,10 @@ st.set_page_config(page_title="Predicting Amazon Stock Prices Using Headlines", 
 
 #Title
 st.title("Predicting Amazon Stock Prices Using Headlines")
-     
+
+#User input
+headline_input = st.text_area("Enter one or more headlines (separated by new lines):")
+current_price = st.number_input("Enter today's price of Amazon stock:", min_value=0.0, format="%.2f")
 
 
 
